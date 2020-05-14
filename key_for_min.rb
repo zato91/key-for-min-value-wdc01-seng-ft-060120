ebc8b440e3ae65179763a53970ee6073e9f4ed4d
zara ahmadi <zatokey@gmail.com>
2020-05-14 18:18:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_key = nil
+  min_val = 0
   name_hash.each do |key,value|
-    value 
+      if min_val < value 
   end
 end
