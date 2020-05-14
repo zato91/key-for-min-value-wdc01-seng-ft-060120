e83de8a50cd6fa5c97c85df03c08a2f43f8dfded
zara ahmadi <zatokey@gmail.com>
2020-05-14 18:20:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,11 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_val = 0
   name_hash.each do |key,value|
-      if min_val < value 
+      if value < min_val
+        min_val = value 
+        min_key = key
+      end
+      
   end
+  min_key
 end
